0afbdf038c228b7ef9ff79e571737dd9ec538273
cmodha <74353527+cmodha@users.noreply.github.com>
2021-08-31 14:09:35 +0100

    Add files via upload

---
+++ KMeansClustering/KMeansClustering.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sklearn
+from sklearn.preprocessing import scale
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+digits = load_digits()
+data = scale(digits.data)
+y = digits.target
+#this should be equal to 10, 1 for each digit
+k = len(np.unique(y))
+print(k)
+#print(data)
+samples,features = data.shape
+
+def bench_k_means(estimator, name, data):
+    estimator.fit(data)
+    print('%-9s\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
+          % (name, estimator.inertia_,
+             metrics.homogeneity_score(y, estimator.labels_),
+             metrics.completeness_score(y, estimator.labels_),
+             metrics.v_measure_score(y, estimator.labels_),
+             metrics.adjusted_rand_score(y, estimator.labels_),
+             metrics.adjusted_mutual_info_score(y,  estimator.labels_),
+             metrics.silhouette_score(data, estimator.labels_,
+                                      metric='euclidean')))
+classifier = KMeans(n_clusters = k,init = 'random',n_init = 100,max_iter=1000, verbose = True)
+bench_k_means(classifier,"KMeans Test",data)
